08807ca36930d07d944e8c7c0010307a6b626812
yahya sleiman <miazfield@gmail.com>
2023-06-20 12:03:06 -0400

    yahya changes

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,6 +94,18 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question = models.TextField()
+    grade_point = models.IntegerField()
+
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
@@ -115,6 +127,10 @@ class Enrollment(models.Model):
     #    else:
     #        return False
 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.TextField()
+    is_correct = models.BooleanField(default=False)
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
@@ -128,7 +144,8 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
